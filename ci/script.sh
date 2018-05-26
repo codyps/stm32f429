567119875e84d2078a415c92d9f9c63e1702b003
Astro <astro@spaceboyz.net>
2018-05-27 00:59:15 +0200

    ci: replace dos2unix with tr

--- ci/script.sh
+++ ci/script.sh
@@ -1,20 +1,22 @@
 set -ex
 
+dos2unix() {
+    tr -s '\r' '\n'
+}
+
 main() {
     local svd=STMicro/STM32F429.svd
 
     cargo check
 
     # check than the patch can be applied to the original SVD
-    local url=https://github.com/posborne/cmsis-svd/raw/python-0.4/data/$svd
-    local td=$(mktemp -d)
+    local url=https://github.com/tones111/cmsis-svd/raw/stm32f4/data/$svd
     local svd=$(basename $svd)
     local patch="${svd%.*}.patch"
     curl -LO $url
-    dos2unix $svd
-    patch -p1 $svd < $patch
-
-    rm -rf $td
+    dos2unix < $svd > $svd.nl
+    mv $svd.nl $svd
+    dos2unix < $patch | patch -p1 $svd
 }
 
 main
