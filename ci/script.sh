f028269a256e4369d496d8a24e1176a453773ddf
Astro <astro@spaceboyz.net>
2018-05-27 00:37:07 +0200

    ci: remove xargo, always use cargo

--- ci/script.sh
+++ ci/script.sh
@@ -3,22 +3,18 @@ set -ex
 main() {
     local svd=STMicro/STM32F429.svd
 
-    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        cargo check
+    cargo check
 
-        # check than the patch can be applied to the original SVD
-        local url=https://github.com/posborne/cmsis-svd/raw/python-0.4/data/$svd
-        local td=$(mktemp -d)
-        local svd=$(basename $svd)
-        local patch="${svd%.*}.patch"
-        curl -LO $url
-        dos2unix $svd
-        patch -p1 $svd < $patch
+    # check than the patch can be applied to the original SVD
+    local url=https://github.com/posborne/cmsis-svd/raw/python-0.4/data/$svd
+    local td=$(mktemp -d)
+    local svd=$(basename $svd)
+    local patch="${svd%.*}.patch"
+    curl -LO $url
+    dos2unix $svd
+    patch -p1 $svd < $patch
 
-        rm -rf $td
-    else
-        xargo check --target $TARGET
-    fi
+    rm -rf $td
 }
 
 main
